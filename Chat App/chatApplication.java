c10498305ca7e59d121714314715fe527ee006fa
Monica Phan <monica.phan.7426@gmail.com>
2019-04-26 19:38:30 -0500

    Add files via upload

--- Chat App/chatApplication.java
+++ Chat App/chatApplication.java
@@ -1,6 +1,7 @@
 // Run me after ServerTest.java- this runs the Client.java code
 // Make sure other device has Client.java and chatApplication.java
 // ipv4 address of my mac on resNet: 10.122.172.184
+// go to https://www.whatismyip.com/
 // local loopback 127.0.0.1
 
 
@@ -10,7 +11,7 @@ import javax.swing.JFrame;
 public class chatApplication{
     public static void main (String[] args){
         Client lunch;
-        lunch = new Client("10.122.172.184"); //local host for testing since using same cpu as server for now
+        lunch = new Client("127.0.0.1"); //local host for testing since using same cpu as server for now
         lunch.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // launch server
         lunch.runApp(); // from Client.java
